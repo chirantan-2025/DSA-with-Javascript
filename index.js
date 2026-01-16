6acc812704c5a5717123f700cd7d732c0cb23a47
chirantan-2025 <chirantanbramhachari560@gmail.com>
2026-01-16 23:55:42 +0530

    segregrate 0s and 1s done

--- index.js
+++ index.js
@@ -67,21 +67,49 @@ const twoSum = function (nums, target) {
   return null;
 };
 
-rl.question("Enter Numbers : ", (numInput) => {
-  const nums = numInput.split(" ").map(Number);
-  rl.question("Enter Target : ", (targetInput) => {
-    const target = Number(targetInput);
-    const result = twoSum(nums, target);
-    if (result) {
-      console.log(`Indices: ${result}`);
-      console.log(
-        `Values: ${nums[result[0] - 1]} + ${
-          nums[result[1] - 1]
-        } = ${target}`
-      );
+// rl.question("Enter Numbers : ", (numInput) => {
+//   const nums = numInput.split(" ").map(Number);
+//   rl.question("Enter Target : ", (targetInput) => {
+//     const target = Number(targetInput);
+//     const result = twoSum(nums, target);
+//     if (result) {
+//       console.log(`Indices: ${result}`);
+//       console.log(
+//         `Values: ${nums[result[0] - 1]} + ${
+//           nums[result[1] - 1]
+//         } = ${target}`
+//       );
+//     } else {
+//       console.log("No solution found");
+//     }
+//     rl.close();
+//   });
+// });
+
+// Segregrate 0s and 1s
+const segregrate0s1s = function (arr) {
+  let i = 0;
+  let j = arr.length - 1;
+  while (i < j) {
+    if (arr[i] === 0) {
+      i++;
+    } else if (arr[j] === 1) {
+      j--;
     } else {
-      console.log("No solution found");
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+      j--;
     }
-    rl.close();
-  });
+  }
+  return arr;
+};
+rl.question("Enter Array: ", (arrInput) => {
+  const arr = arrInput.split(" ").map(Number);
+  const result = segregrate0s1s(arr);
+  if (result) {
+    console.log(`${result}`);
+  } else {
+    console.log("No result found");
+  }
+  rl.close();
 });
